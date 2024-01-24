e8400150721ac8fd15c65a1b0713cb2e4154be05
Jakob Hofer <jakob.refoh@gmail.com>
2024-01-24 11:48:47 +0100

    fix some consistency errors in pp and remoce seccomp sontainers after they exit

--- Code examples/seccomp/run.sh
+++ Code examples/seccomp/run.sh
@@ -2,10 +2,15 @@
 
 docker run \
   --security-opt seccomp=profile.json \
+  --name with_seccomp \
+  --rm \
   ubuntu \
   mkdir test
 
 docker run \
   --security-opt seccomp=unconfined \
+  --name unconfined \
+  --rm \
   ubuntu \
   sh -c "apt update && apt install -y gdb && gdb echo"
+
